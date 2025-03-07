4ca938bf8463ee0333247d66109b3681dbecf305
raf <rraf@tuta.io>
2025-03-07 15:40:10 +0000

    chore: added aws & webstorm

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  USER = "raf";
+in
 {
   homebrew = {
     casks = [
       "intellij-idea"
+      "webstorm"
       "microsoft-teams"
       "microsoft-outlook"
       "docker"
@@ -11,4 +15,11 @@
     brews = [
     ];
   };
+
+  home-manager.users.${USER}.home.packages =
+    with pkgs;
+    [
+      awscli2
+    ]
+    ++ (if pkgs.stdenv.isDarwin then [ ] else [ ]);
 }
